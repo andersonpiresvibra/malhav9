1d2a9bc01f6a9b1969a98f97defab9bac53cc91a
andersonpiresvibra <andersonpires.vibra@gmail.com>
2026-05-21 14:28:43 -0300

    refactor: remove unused path import from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath } from "url"
-import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
