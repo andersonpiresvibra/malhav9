892f1ce1075cc88e06d9c2a10a4deee53d821667
andersonpiresvibra <andersonpires.vibra@gmail.com>
2026-05-21 14:38:02 -0300

    refactor: remove Hono and wrap app with providers
    Remove Hono dependencies as they are no longer in use and wrap the application with AuthProvider and ThemeProvider to manage global state.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import { AuthProvider } from './contexts/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>,
 );
+
